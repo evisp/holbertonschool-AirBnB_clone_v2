ae645323c843d7499e1906c8cf0cbd055c228d6e
evisp <evisplaku@gmail.com>
2022-08-09 10:50:20 -0700

    added: back_populates

--- models/place.py
+++ models/place.py
@@ -56,6 +56,7 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
+                             back_populates="place_amenities",
                              viewonly=False)
     amenity_ids = []
 
